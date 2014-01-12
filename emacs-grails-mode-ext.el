2d7375be3c42da3dd3b344e7e2dce734a2d06662
Yves Zoundi <yveszoundi@gmail.com>
2014-01-12 15:55:05 -0500

    Updates and cleanup.

--- emacs-grails-mode-ext.el
+++ emacs-grails-mode-ext.el
@@ -1,13 +1,21 @@
-;; ================================
-;; Extensions for grails-mode
-;; Author : Yves Zoundi
-;; Created : 08-Dec-2012
+;;; emacs-grails-mode-ext.el --- Extensions for grails-mode.
+;;
+;;
+;; Author: Yves Zoundi <rimerosolutions@gmail.com>
+;; Maintainer: Yves Zoundi
+;; Version: 20140109.190629
+;; X-Original-Version: 1.0.0
+;; Package-Requires: ((grails-mode "0.1") (emacs "24"))
+;; Contributors: The internet and people who surf it.
+;; Last updated: 2014-01-09
+;;
+;; This file is not part of GNU Emacs.
 ;;
 ;; License: GNU GPL v3 (http://www.gnu.org/licenses/gpl-3.0.txt)
 ;; Sypnosis:
-;;    - You can run pre-defined or arbitrary Grails commans for a project
-;;    - You can browse documentation (wiki, guide, apidocs)
-;;    - Menubar contributions in Grails mode
+;;    - You can run pre-defined or arbitrary Grails commands for a project.
+;;    - You can browse documentation (wiki, guide, apidocs).
+;;    - Menubar contributions in Grails mode.
 ;;
 ;; To list keybindings press C-h b and search for grails-mode.
 ;;
@@ -15,121 +23,154 @@
 (require 'grails-mode)
 (grails-mode +1)
 
+(defcustom grails-wrapper-filename "grailsw"
+  "Grails Wrapper file name."
+  :type 'string
+  :group 'grails)
+
+(defcustom grails-output-opts "--plain-output"
+  "Output without weird characters."
+  :type 'string
+  :group 'grails)
+
+(defcustom grails-jvm-opts "-DXmx1g"
+  "Grails command line options"
+  :type 'string
+  :group 'grails)
+
+(defconst grails-cmp-buffer-name "*Grails*"
+  "Grails commands buffer name")
+
+(defcustom grails-use-wrapper-when-possible t
+  "Use the Grails wrapper whenever available."
+  :type 'boolean
+  :group 'grails)
+
+(defcustom grails-executable-path-no-ext "grails"
+  "Path to the Grails executable without filename extension."
+  :type 'string
+  :group 'grails)
+
+(defcustom grails-executable-suffix
+  (if (eq system-type 'windows-nt)
+      ".bat" "")
+  "Suffix for the Grails executable file.")
+
 ;; --------------------------------
 ;; Main functions
 ;; --------------------------------
-(defun grails/command (str)
-  "Run a Grails command (Non interactive)"
-
+(defun grails--command (str)
+  "Run a Grails command (Non interactive)."
   (project-ensure-current)
-  (setq shell-command-initial-directory (project-default-directory (project-current)))
+  (let ((default-directory (project-default-directory (project-current)))
+        (grails-executable (concat grails-executable-path-no-ext grails-executable-suffix))
+        (grails-wrapper-path (concat default-directory grails-wrapper-filename grails-executable-suffix)))
 
-  ;; store old directory
-  (setq old-dir default-directory)
+    (when (and grails-use-wrapper-when-possible
+               (file-exists-p grails-wrapper-path))
+      (setq grails-executable grails-wrapper-path))
 
-  ;; set the new working directory for the command
-  (setq command-initial-directory (expand-file-name shell-command-initial-directory))
-  (cd command-initial-directory)
+    (let ((grails-command-line (concat grails-executable
+                                       " "
+                                       grails-output-opts
+                                       " "
+                                       str)))
+      (async-shell-command grails-command-line grails-cmp-buffer-name))))
 
-  ;; runs the grails command from the project directory
-  (async-shell-command (concat "grails " str) "*Grails*")
-
-  ;; restore previous directory
-  (cd old-dir))
-
-(defun grails/read-param-and-run (input-hint grails-command)
-  "Read an input parameter and invoke a given Grails command"
-  (setq grails-command-argument (read-from-minibuffer input-hint))
-
-  (grails/command (concat grails-command " " grails-command-argument)))
+(defun grails--read-param-and-run (input-hint grails-command)
+  "Read an input parameter and invoke a given Grails command."
+  (let ((grails-command-argument (read-from-minibuffer input-hint)))
+    (grails--command (concat grails-command " " grails-command-argument))))
 
 ;; --------------------------------
 ;; General functions
 ;; --------------------------------
-(defun grails/icommand ()
-  "Enter a Grails command (Interactive)"
+(defun grails-icommand ()
+  "Enter a Grails command (Interactive)."
   (interactive)
-  (grails/read-param-and-run "Goal:" ""))
+  (grails--read-param-and-run "Goal:" ""))
 
-(defun grails/create-domain ()
-  "Create a Grails Domain Class"
+(defun grails-create-domain ()
+  "Create a Grails Domain Class."
   (interactive)
-  (grails/read-param-and-run "Domain class:" "create-domain-class"))
+  (grails--read-param-and-run "Domain class:" "create-domain-class"))
 
-(defun grails/create-controller ()
-  "Create a Grails Controller"
+(defun grails-create-controller ()
+  "Create a Grails Controller."
   (interactive)
-  (grails/read-param-and-run "Controller Domain class:" "create-controller"))
+  (grails--read-param-and-run "Controller Domain class:" "create-controller"))
 
-(defun grails/create-service ()
-  "Create a Grails Service"
+(defun grails-create-service ()
+  "Create a Grails Service."
   (interactive)
-  (grails/read-param-and-run "Service Domain class:" "create-service"))
+  (grails--read-param-and-run "Service Domain class:" "create-service"))
 
-(defun grails/create-taglib ()
-  "Create a Grails Taglib"
+(defun grails-create-taglib ()
+  "Create a Grails Taglib."
   (interactive)
-  (grails/read-param-and-run "TagLib Name:" "create-tag-lib"))
+  (grails--read-param-and-run "TagLib Name:" "create-tag-lib"))
 
 ;; --------------------------------
 ;; Plugin functions
 ;; --------------------------------
-(defun grails/install-plugin ()
-  "Install a Grails plugin"
+(defun grails-install-plugin ()
+  "Install a Grails plugin."
   (interactive)
-  (grails/read-param-and-run "name optionalversion:" "install-plugin"))
+  (grails--read-param-and-run "name optionalversion:" "install-plugin"))
 
-(defun grails/uninstall-plugin ()
-  "Uninstall a Grails plugin"
+(defun grails-uninstall-plugin ()
+  "Uninstall a Grails plugin."
   (interactive)
-  (grails/read-param-and-run "Plugin Name:" "uninstall-plugin"))
+  (grails--read-param-and-run "Plugin Name:" "uninstall-plugin"))
 
-(defun grails/package-plugin ()
-  "Package a Grails plugin"
+(defun grails-package-plugin ()
+  "Package a Grails plugin."
   (interactive)
-  (grails/command "package-plugin"))
+  (grails--command "package-plugin"))
 
-(defun grails/refresh-dependencies ()
-  "Refresh Grails Dependencies"
+(defun grails-refresh-dependencies ()
+  "Refresh Grails Dependencies."
   (interactive)
-  (grails/command "refresh-dependencies"))
+  (grails--command "refresh-dependencies"))
 
 ;; --------------------------------
 ;; Browse docs (api, wiki, guide)
 ;; --------------------------------
-(defun grails/browse-wiki-docs ()
-  "Browse the Wiki Documentation"
+(defun grails-browse-wiki-docs ()
+  "Browse the Wiki Documentation."
   (interactive)
   (browse-url "http://grails.org/Documentation"))
 
-(defun grails/browse-api-docs ()
-  "Browse the API Documentation"
+(defun grails-browse-api-docs ()
+  "Browse the API Documentation."
   (interactive)
   (browse-url "http://grails.org/doc/latest/api/"))
 
-(defun grails/browse-latest-guide ()
-  "Browse the official Grails Guide"
+(defun grails-browse-latest-guide ()
+  "Browse the official Grails Guide."
   (interactive)
   (browse-url "http://grails.org/doc/latest/guide/single.html"))
 
-(defun grails/contribute-keys()
+(defun grails-contribute-keys()
   "Add keybindings"
-  (global-set-key   (kbd "C-c ;e")  'grails/icommand)
-  (global-set-key   (kbd "C-c ;cd") 'grails/create-domain)
-  (global-set-key   (kbd "C-c ;cc") 'grails/create-controller)
-  (global-set-key   (kbd "C-c ;cs") 'grails/create-service) )
+  (global-set-key   (kbd "C-c ;e")  'grails-icommand)
+  (global-set-key   (kbd "C-c ;cd") 'grails-create-domain)
+  (global-set-key   (kbd "C-c ;cc") 'grails-create-controller)
+  (global-set-key   (kbd "C-c ;cs") 'grails-create-service) )
 
 (eval-after-load "emacs-grails-mode-ext"
-  '(progn (grails/contribute-keys) ))
+  '(progn (grails-contribute-keys) ))
 
-(defun grails/contribute-menu()
-  "Add menu extensions"
-  (define-key global-map [menu-bar grailmenu grails-cmd-1] '("Execute Command"   . grails/icommand))
-  (define-key global-map [menu-bar grailmenu grails-cmd-2] '("Create Domain"     . grails/create-domain))
-  (define-key global-map [menu-bar grailmenu grails-cmd-3] '("Create Controller" . grails/create-controller))
-  (define-key global-map [menu-bar grailmenu grails-cmd-4] '("Create Service"    . grails/create-service)))
+(defun grails-contribute-menu()
+  "Add menu extensions."
+  (define-key global-map [menu-bar grailmenu grails-cmd-1] '("Execute Command"   . grails-icommand))
+  (define-key global-map [menu-bar grailmenu grails-cmd-2] '("Create Domain"     . grails-create-domain))
+  (define-key global-map [menu-bar grailmenu grails-cmd-3] '("Create Controller" . grails-create-controller))
+  (define-key global-map [menu-bar grailmenu grails-cmd-4] '("Create Service"    . grails-create-service)))
 
-(add-hook 'menu-bar-update-hook 'grails/contribute-menu)
-(grails/contribute-menu)
+(add-hook 'menu-bar-update-hook 'grails-contribute-menu)
+(grails-contribute-menu)
 
 (provide 'emacs-grails-mode-ext)
+
+;;; emacs-grails-mode-ext.el ends here
--- grails-projectile-mode.el
+++ grails-projectile-mode.el
@@ -2,13 +2,14 @@
 ;;
 ;; Copyright (C) 2013 Rimero Solutions
 ;;
-;; Version: 20131201.084654
+;; Version: 20140109.190629
 ;; X-Original-Version: 1.0.0
 ;; Keywords: elisp, grails, projectile
 ;; Author: Yves Zoundi <rimerosolutions@gmail.com>
 ;; Maintainer: Yves Zoundi
+;; Package-Requires: ((projectile "0.8") (emacs "24"))
 ;; Contributors: The internet and people who surf it.
-;; Last updated: 2013-12-01
+;; Last updated: 2014-01-09
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -54,13 +55,14 @@
 
 (defcustom grails-projectile-keymap-prefix (kbd "C-c ;")
   "Grails Projectile keymap prefix."
-  :group 'projectile
-  :type 'string)
+  :group 'grails-projectile
+  :type 'string
+  :link '(url-link :tag "Github" ""))
 
 (defcustom grails-projectile-mode-line " Grails"
   "Grails projectile modeline."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defvar grails-executable-suffix
   (if (eq system-type 'windows-nt)
@@ -70,65 +72,65 @@
 (defcustom grails-compilation-buffer-name "*Grails*"
   "Buffer name for Grails commands."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom use-grails-wrapper-when-possible t
   "Use the Grails wrapper whenever available."
   :type 'boolean
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-output-opts ""
   "Output options such as --plain-output."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-cmd-opts "--non-interactive --stacktrace"
   "Grails command line options."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-wrapper-filename "grailsw"
   "Grails Wrapper file name."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-projectile-filename ".grails-projectile"
   "Project file to define custom grails command and JVM options.
    The contents of this file override both grails-cmd-opts and grails-jvm-opts.
    Everything must hold within a single line, no newline at the end of the file."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-jvm-opts "-DXmx1g"
   "Grails command line options"
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-executable "grails"
   "Path to Grails executable.
   By default, it's assumed that grails is in your PATH variable."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-url-wikidocs "http://grails.org/Documentation"
   "Grails Wiki documentation URL."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-url-apidocs "http://grails.org/doc/latest/api/"
   "Grails documentation URL."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-plugins-base-url "http://grails.org/plugins/"
   "Grails plugins base URL."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defcustom grails-url-guide "http://grails.org/doc/latest/guide/single.html"
   "Grails Latest Guide URL."
   :type 'string
-  :group 'grails)
+  :group 'grails-projectile)
 
 (defun grails/--join-lines (beg end)
   "Apply join-line over region."
@@ -163,7 +165,6 @@
 
 (defun grails/--wizard-new-app-or-plugin (cmd)
   "Create a new application or plugin."
-
   (let ((insert-default-directory  t))
     ;; Ask the user for the project folder
     (let ((grails-project-folder (read-directory-name "Application Directory: " default-directory))
@@ -192,19 +193,25 @@
 ;; --------------------------------
 ;; Finder helper functions
 ;; --------------------------------
-(defun grails/--find-grails-file (grails-proj-folder pred-fn-sym file-basename)
+(defun grails/--find-grails-file (grails-proj-folder pred-fn-sym file-basename &optional no-auto-open)
   "Find a Grails file in a project folder.
 
    grails-proj-folder is the base search folder.
    pred-fn-sym is the function to filter project files.
    file-basename is the filename to search without extension.
+   no-auto-open Do not open the file automatically for a single result.
 "
   (let ( (result-list (grails/--find-grails-files grails-proj-folder
                                                   file-basename
                                                   pred-fn-sym)))
     (if result-list
         (if (= (length result-list) 1)
-            (find-file (concat (projectile-project-root) (car result-list)))
+            (if (not no-auto-open)
+                (find-file (concat (projectile-project-root) (car result-list)))
+              (progn
+                (let ((file-list (mapcar #'(lambda(p) (concat (projectile-project-root) p)) result-list)))
+                  (let ((selected-file (completing-read "Select a file:" file-list)))
+                    (find-file selected-file)))))
           (progn
             (let ((file-list (mapcar #'(lambda(p) (concat (projectile-project-root) p)) result-list)))
               (let ((selected-file (completing-read "Select a file:" file-list)))
@@ -212,17 +219,16 @@
       (message "No artefact found for %s in '%s'" file-basename grails-proj-folder))))
 
 (defun grails/--find-grails-files (dirname file-basename pred-fn)
-  "Jumps to a filename from a given base folder."
-
-  (let ((folder-files (projectile-files-in-project-directory dirname)))
-    (let ((filtered-folder-files '()))
-      (dolist (elt folder-files)
-        (when (funcall pred-fn (file-name-base elt) file-basename)
-          (add-to-list 'filtered-folder-files elt)))
-      filtered-folder-files)))
+  "Jump to a filename from a given base folder."
+  (let ((folder-files (projectile-files-in-project-directory dirname))
+        (filtered-folder-files '())
+        (dolist (elt folder-files)
+          (when (funcall pred-fn (file-name-base elt) file-basename)
+            (add-to-list 'filtered-folder-files elt)))
+        filtered-folder-files)))
 
 (defun grails/--base-name-matches-p (value expected)
-  "Matches two strings."
+  "Match two strings."
   (string= expected value))
 
 (defun grails/--test-matches-p (value expected)
@@ -230,14 +236,18 @@
   (or (string= (concat expected "Tests") value)
       (string= (concat expected "Spec") value)))
 
+(defun grails/--all-files (value expected)
+  "Test whether a file should be opened."
+  t)
+
 (defun grails/--string/ends-with (s ending)
-  "return non-nil if string S ends with ENDING."
+  "Return non-nil if string S ends with ENDING."
   (let ((elength (length ending)))
     (if (< elength (length s))
         (string= (substring s (- 0 elength)) ending))))
 
 (defun grails/--artefact-name-no-suffix (file-basename)
-  "Returns the Grails artefact name without its suffix
+  "Return the Grails artefact name without its suffix
 
   file-basename is the full basename of the file such as TestController.
 
@@ -269,12 +279,13 @@
 ;; --------------------------------
 ;; Finder functions
 ;; --------------------------------
-(defun grails/locate-test (test-name)
+(defun grails/locate-test ()
   "Locate a test class in the project."
-  (interactive "sTest file name without extension: \n")
+  (interactive)
   (grails/--find-grails-file (grails/--project-sub-folder "test")
-                             'grails/--test-matches-p
-                             test-name))
+                             'grails/--all-files
+                             ""
+                             t))
 
 (defun grails/find-test-for-file ()
   "Find a test class associated with the current file."
@@ -283,44 +294,65 @@
                              'grails/--test-matches-p
                              (file-name-base (buffer-file-name))))
 
-(defun grails/locate-service (service-name)
+(defun grails/locate-view ()
+  "Locate a view in the project."
+  (interactive)
+  (grails/--find-grails-file (grails/--grails-app-folder "views")
+                             'grails/--all-files
+                             ""
+                             t))
+
+(defun grails/locate-service ()
   "Locate a service class in the project."
-  (interactive "sService full-name: \n")
-  (grails/--find-artefact "services" "Service" service-name))
+  (interactive)
+  (grails/--find-grails-file (grails/--grails-app-folder "services")
+                             'grails/--all-files
+                             ""
+                             t))
 
 (defun grails/find-service-for-file ()
   "Find a service class associated with the current file."
-  (interactive)  
+  (interactive)
   (grails/--find-artefact "services" "Service"))
 
-(defun grails/locate-controller (controller-name)
+(defun grails/locate-controller ()
   "Locate a controller class in the project."
-  (interactive "sController full-name: \n")
-  (grails/--find-artefact "controllers" "Controller" controller-name))
+  (interactive)
+  (grails/--find-grails-file (grails/--grails-app-folder "controllers")
+                             'grails/--all-files
+                             ""
+                             t))
 
 (defun grails/find-controller-for-file ()
   "Find a controller class associated with the current file."
-  (interactive)  
+  (interactive)
   (grails/--find-artefact "controllers" "Controller"))
 
-(defun grails/locate-domain (domain-name)
+(defun grails/locate-domain ()
   "Locate a domain class in the project."
-  (interactive "sDomain name: \n")
-  (grails/--find-artefact "domain" "" domain-name))
+  (interactive)
+
+  (grails/--find-grails-file (grails/--grails-app-folder "domain")
+                             'grails/--all-files
+                             ""
+                             t))
 
 (defun grails/find-domain-for-file ()
   "Find a domain class associated with the current file."
   (interactive)
   (grails/--find-artefact "domain" ""))
 
-(defun grails/locate-taglib (tag-lib-name)
+(defun grails/locate-taglib ()
   "Locate a taglib class in the project."
-  (interactive "sTagLib full-name: \n")
-  (grails/--find-artefact "taglib" "TagLib" tag-lib-name))
+  (interactive)
+  (grails/--find-grails-file (grails/--grails-app-folder "taglib")
+                             'grails/--all-files
+                             ""
+                             t))
 
 (defun grails/find-taglib-for-file ()
   "Find a taglib class associated to the current file."
-  (interactive)  
+  (interactive)
   (grails/--find-artefact "taglib" "TagLib"))
 
 ;; --------------------------------
@@ -332,7 +364,7 @@
 
 (defun grails/--grails-app-folder (folder-name)
   "grails-app sub-folder path of the project."
-  (grails/--project-sub-folder "grails-app"))
+  (concat (grails/--project-sub-folder "grails-app") folder-name))
 
 (defun grails/--wrapper-exists-p (folder-name)
   "Check whether the Grails wrapper exist in a given folder."
@@ -344,12 +376,14 @@
         (grails-args (concat grails-jvm-opts " " grails-cmd-opts))
         (grails-cmd-line (concat grails-executable grails-executable-suffix)))
 
-    (when use-grails-wrapper-when-possible
-      (when (grails/--wrapper-exists-p default-directory)
-        (setq grails-cmd-line (concat default-directory grails-wrapper-filename grails-executable-suffix))))
+    (when (and use-grails-wrapper-when-possible
+               (grails/--wrapper-exists-p default-directory))
+      (let ((grailsw-file (concat default-directory grails-wrapper-filename grails-executable-suffix)))
+        (setq grails-cmd-line grailsw-file)))
 
     (when (file-exists-p (concat default-directory grails-projectile-filename))
-      (setq grails-args (grails/--read-grails-options-projectile-file (concat default-directory grails-projectile-filename))))
+      (let ((grails-projectile-file (concat default-directory grails-projectile-filename)))
+        (setq grails-args (grails/--read-grails-options-projectile-file grails-projectile-file))))
 
     (concat grails-cmd-line " " grails-output-opts " " grails-args " " grails-command)))
 
@@ -358,10 +392,9 @@
 ;; --------------------------------
 (defun grails/--command (str)
   "Run a Grails command."
-
-  (let ((grails-command-line (grails/--get-cmd str)))
-    (let ((default-directory (expand-file-name (projectile-project-root))))
-      (compilation-start grails-command-line 'compilation-mode 'grails/--get-compilation-buffer-name))))
+  (let ((grails-command-line (grails/--get-cmd str))
+        (default-directory (expand-file-name (projectile-project-root))))
+    (compilation-start grails-command-line 'compilation-mode 'grails/--get-compilation-buffer-name)))
 
 (defun grails/--get-compilation-buffer-name (mode)
   "The buffer name to use for Grails Commands."
@@ -369,7 +402,6 @@
 
 (defun grails/--read-param-and-run (input-hint grails-command)
   "Read an input parameter and invoke a given Grails command."
-
   (let (grails-command-argument)
     (setq grails-command-argument (read-from-minibuffer input-hint))
     (grails/--command (concat grails-command " " grails-command-argument))))
@@ -379,7 +411,6 @@
 ;; --------------------------------
 (defun grails/icommand ()
   "Enter a Grails command."
-
   (interactive)
   (grails/--read-param-and-run "Goal:" ""))
 
@@ -389,20 +420,17 @@
   (grails/--read-param-and-run "Domain class:" "create-domain-class"))
 
 (defun grails/create-controller ()
-  "Create a Grails Controller"
-
+  "Create a Grails Controller."
   (interactive)
   (grails/--read-param-and-run "Controller Domain class:" "create-controller"))
 
 (defun grails/create-service ()
   "Create a Grails Service."
-
   (interactive)
   (grails/--read-param-and-run "Service Domain class:" "create-service"))
 
 (defun grails/create-taglib ()
   "Create a Grails Taglib."
-
   (interactive)
   (grails/--read-param-and-run "TagLib Name:" "create-tag-lib"))
 
@@ -411,13 +439,11 @@
 ;; --------------------------------
 (defun grails/plugins-list-installed ()
   "List Grails installed plugins."
-
   (interactive)
   (grails/--command "list-plugins -installed"))
 
 (defun grails/plugins-package-plugin ()
   "Package a Grails plugin."
-
   (interactive)
   (grails/--command "package-plugin"))
 
@@ -426,19 +452,16 @@
 ;; --------------------------------
 (defun grails/compile ()
   "Compile."
-
   (interactive)
   (grails/--command "compile"))
 
 (defun grails/clean ()
   "Clean."
-
   (interactive)
   (grails/--command "clean"))
 
 (defun grails/refresh-dependencies ()
   "Refresh Grails Dependencies."
-
   (interactive)
   (grails/--command "refresh-dependencies"))
 
@@ -447,7 +470,6 @@
 ;; --------------------------------
 (defun grails/browse-wiki-docs ()
   "Browse the Wiki Documentation."
-
   (interactive)
   (if (boundp 'grails-url-wikidocs)
       (browse-url grails-url-wikidocs)
@@ -455,7 +477,6 @@
 
 (defun grails/browse-api-docs ()
   "Browse the API Documentation."
-
   (interactive)
   (if (boundp 'grails-url-apidocs)
       (browse-url grails-url-apidocs)
@@ -470,22 +491,21 @@
   (interactive "sPlugin name or query: \n")
   (if (boundp 'grails-plugins-base-url)
       (grails/--search-plugin grails-plugins-base-url query-string)
-    (message "No Grails plugins base URL set. Customize the 'grails' group")))
+    (message "No Grails plugins base URL set. Customize the 'grails-projectile' group")))
 
 (defun grails/search-plugin-tag (query-string)
   "Search Grails plugins."
   (interactive "sPlugin tag: \n")
   (if (boundp 'grails-plugins-base-url)
       (grails/--search-plugin ((concat grails-plugins-base-url "tag/") query-string))
-    (message "No Grails plugins base URL set. Customize the 'grails' group")))
+    (message "No Grails plugins base URL set. Customize the 'grails-projectile' group")))
 
 (defun grails/browse-latest-guide ()
   "Browse the official Grails Guide."
-
   (interactive)
   (if (boundp 'grails-url-guide)
       (browse-url grails-url-guide)
-    (message "No Grails URL guide set. Customize the 'grails' group")))
+    (message "No Grails URL guide set. Customize the 'grails-projectile' group")))
 
 
 ;;; Minor mode
@@ -510,6 +530,7 @@
       (define-key prefix-map   (kbd "l d") 'grails/locate-domain)
       (define-key prefix-map   (kbd "l t") 'grails/locate-test)
       (define-key prefix-map   (kbd "l s") 'grails/locate-service)
+      (define-key prefix-map   (kbd "l v") 'grails/locate-view)
       (define-key prefix-map   (kbd "l c") 'grails/locate-controller)
 
       (define-key prefix-map   (kbd "n a") 'grails/wizard-new-app)
@@ -564,7 +585,6 @@
   (defadvice projectile-default-test-command
       (around grails-projectile-default-test-command (project-type) )
     "After execution of projectile-default-test-command."
-
     (if (eq project-type 'grails)
         (setq ad-return-value (grails/--get-cmd "test-app"))
       ad-do-it))
@@ -572,7 +592,6 @@
   (defadvice projectile-default-compilation-command
       (around grails-projectile-default-compilation-command (project-type) )
     "After execution of projectile-default-compilation-command."
-
     (if (eq project-type 'grails)
         (setq ad-return-value (grails/--get-cmd "compile"))
       ad-do-it))
@@ -580,11 +599,9 @@
   (defadvice projectile-project-type
       (around grails-projectile-project-type () )
     "After execution of projectile-project-type."
-
     (if (projectile-verify-files projectile-grails-spec)
         (setq ad-return-value 'grails)
-      ad-do-it))
-  )
+      ad-do-it)))
 
 ;;;###autoload
 (define-minor-mode grails-projectile-mode
@@ -593,7 +610,7 @@
   \\{grails-projectile-mode-map}"
   :lighter grails-projectile-mode-line
   :keymap  'grails-projectile-mode-map
-  :group   'grails
+  :group   'grails-projectile
   :require 'grails-projectile-mode
 
   (progn
@@ -631,3 +648,5 @@
   (grails-projectile-global-mode -1))
 
 (provide 'grails-projectile-mode)
+
+;;; grails-projectile-mode.el ends here
